84641d923ddcc298a2f6e7f63af0c728657bfc9f
Maksim <Maksim>
2017-05-17 15:13:16 +0300

    change#!

--- src/bin/baranin-games.js
+++ src/bin/baranin-games.js
@@ -1,6 +1,6 @@
-#!/usr/bin/nodejs
+#!/usr/bin/env nodejs
 import { readlineSync } from  'readline-sync';
-import questions  from  '../src'
+//import questions  from  '../src'
 console.log('Welcome to the Brain Games!');
 //const userName = readlineSync.question('May I have user name?: ');
 //questions(userName);
