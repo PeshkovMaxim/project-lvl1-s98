8bb12e8180f9da63a8c2d208000ddfc44e28dc02
Maksim <Maksim>
2017-05-17 14:44:38 +0300

    rm dist

--- dist/bin/baranin-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/nodejs
-'use strict';
-
-console.log('Welcome to the Brain Games!');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYmFyYW5pbi1nYW1lcy5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiOztBQUNBQSxRQUFRQyxHQUFSLENBQVksNkJBQVoiLCJmaWxlIjoiYmFyYW5pbi1nYW1lcy5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzIScpO1xuXG5cblxuIl19
