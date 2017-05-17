19fd0b6fc83bf6a5c5b8d6f9d063c9264d93f802
Maksim <Maksim>
2017-05-17 16:03:44 +0300

    moduleAdd

--- src/bin/baranin-games.js
+++ src/bin/baranin-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env nodejs
 import { readlineSync } from  'readline-sync';
-//import questions  from  '../src'
+import questions  from  '..'
 console.log('Welcome to the Brain Games!');
-//const userName = readlineSync.question('May I have user name?: ');
-//questions(userName);
+const userName = readlineSync.question('May I have user name?: ');
+questions(userName);
