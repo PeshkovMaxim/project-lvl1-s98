e48bf338ecd54b1a0687ace84f9593a66f711043
Maksim <Maksim>
2017-05-18 21:30:12 +0300

    addPackage

---
+++ src/bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env nodejs
+import { startEvenGame } from '../games/brainEvenFunc';
+
+startEvenGame(2, 100);
---
+++ src/games/brainEvenFunc.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+export const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const startEvenGame = (a, b) => {
+  console.log(`Welcome to the Brain Games!
+Answer "yes" if number even otherwise answer "no".\n`);
+
+  const userName = readlineSync.question('May I have user name?: ');
+  console.log(`Hi, ${userName}!`);
+  const startGame = (acc) => {
+    if (acc === 3) {
+      return console.log(`Congratulations, ${userName}!`);
+    }
+    const numQn = getRandom(a, b);
+    console.log(`\nQuestion:${numQn}`);
+    const getAnswer = readlineSync.question('Your answer: ');
+    console.log(getAnswer);
+    if (getAnswer !== 'yes' && getAnswer !== 'no') {
+      return console.log(`'${getAnswer}' is wrong answer ;(. Correct answer was 'yes' or 'no'.`);
+    }
+    if (getAnswer === 'yes' && numQn % 2 === 0) {
+      console.log('Correct!');
+    }
+    if (getAnswer === 'no' && numQn % 2 !== 0) {
+      console.log('Correct!');
+    }
+    if (getAnswer === 'no' && numQn % 2 === 0) {
+      return console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    }
+    if (getAnswer === 'yes' && numQn % 2 !== 0) {
+      return console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    }
+    return startGame(acc + 1);
+  };
+  startGame(0);
+};
