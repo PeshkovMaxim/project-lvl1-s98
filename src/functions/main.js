7c4513d65213a4e7aed86791e7d495be6fe3804e
Maksim <Maksim>
2017-05-20 19:10:15 +0300

    modified GcdGame

--- src/functions/main.js
+++ src/functions/main.js
@@ -1,22 +1,30 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
 
-const startAnyGame = (message, question, answer, acc) => {
+const startAnyGame = (message, generator) => {
   console.log('Welcome to the Brain Games!');
   console.log(message);
   const name = readlineSync.question('May I have user name?: ');
   console.log(`Hi, ${name}!`);
-  if (acc === 3) {
-    return console.log(`Congratulations, ${name}!`);
-  }
-  console.log(`\nQuestion: ${question}`);
-  const getAnswer = readlineSync.question('Your answer: ');
-  if (getAnswer === `${answer}`) {
-    console.log('Correct!');
-  }
-  if (getAnswer !== `${answer}`) {
-    console.log(`'${getAnswer}' is wrong answer ;(. Try later.`);
-    return false;
-  }
-  return true;
+
+  const startGame = (func, acc) => {
+    const game = generator();
+    const question = car(game);
+    const answer = cdr(game);
+    if (acc === 3) {
+      return console.log(`Congratulations, ${name}!`);
+    }
+    console.log(`\nQuestion: ${question}`);
+    const getAnswer = readlineSync.question('Your answer: ');
+    if (getAnswer === `${answer}`) {
+      console.log('Correct!');
+    }
+    if (getAnswer !== `${answer}`) {
+      console.log(`'${getAnswer}' is wrong answer ;(. Try later.`);
+      return false;
+    }
+    return startGame(generator, acc + 1);
+  };
+  return startGame(generator, 0);
 };
 export default startAnyGame;
--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -1,13 +1,14 @@
+import { cons } from 'hexlet-pairs';
 import { gcd, getNumber } from '../functions/mathFunctions';
 import startAnyGame from '../functions/main';
 
 const startGcdGame = () => {
   const startWelcome = 'Find the greatest common divisor of given numbers.\n';
-  const num1 = getNumber(2, 50);
-  const num2 = getNumber(1, 30);
-  const answer = gcd(num1, num2);
-  const question = `${num1} ${num2}`;
-
-  startAnyGame(startWelcome, question, answer);
+  const generator = () => {
+    const num1 = getNumber(2, 50);
+    const num2 = getNumber(1, 30);
+    return cons(`${num1} ${num2}`, gcd(num1, num2));
+};
+  startAnyGame(startWelcome, generator);
 };
 export default startGcdGame;
